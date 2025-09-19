e653e044f78bc61949f3952441434dd82cc0efdc
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-09-19 08:43:32 -0300

    docs: Atualziando application.yml

--- src/main/java/com/example/challenge_mottu/ChallengeMottuApplication.java
+++ src/main/java/com/example/challenge_mottu/ChallengeMottuApplication.java
@@ -1,5 +1,6 @@
 package com.example.challenge_mottu;
 
+import org.apache.catalina.util.ServerInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class ChallengeMottuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeMottuApplication.class, args);
+		System.out.println(ServerInfo.getServerInfo());
 	}
 
 }
