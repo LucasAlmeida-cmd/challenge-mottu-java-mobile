ae9f1551965de109c99742f03a5616c62fbf6e0e
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-09-23 20:22:19 -0300

    docs: Atualiziando application.yml

--- src/main/java/com/example/challenge_mottu/ChallengeMottuApplication.java
+++ src/main/java/com/example/challenge_mottu/ChallengeMottuApplication.java
@@ -6,15 +6,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChallengeMottuApplication extends SpringBootServletInitializer {
-
+public class ChallengeMottuApplication  {
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeMottuApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ChallengeMottuApplication.class);
-	}
 
 }
