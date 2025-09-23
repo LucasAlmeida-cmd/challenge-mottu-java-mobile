7d15e4fe9dcecf097ad2e8987ecb20ad03ddab1f
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-09-23 20:50:10 -0300

    fix

--- src/main/java/com/example/challenge_mottu/ChallengeMottuApplication.java
+++ src/main/java/com/example/challenge_mottu/ChallengeMottuApplication.java
@@ -2,14 +2,12 @@ package com.example.challenge_mottu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChallengeMottuApplication  {
+public class ChallengeMottuApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeMottuApplication.class, args);
 	}
-
 
 }
